0614663ca43b7f48bb19e47f9f962661667d97b3
primocodetoday <primocodetoday@gmail.com>
2020-04-12 15:30:40 +0200

    Added active behaviors & js file

---
+++ scripts/index.js
@@ -0,0 +1,16 @@
+﻿const annually = [199.99, 249.99, 399.99];
+
+const monthly = [19.99, 24.99, 39.99];
+
+const toggle = document.querySelector("input");
+const prices = document.querySelectorAll(".price");
+
+function changePriceMonthly() {
+  if (this.checked) {
+    prices.forEach((price, i) => (price.textContent = monthly[i]));
+  } else {
+    prices.forEach((price, i) => (price.textContent = annually[i]));
+  }
+}
+
+toggle.addEventListener("change", changePriceMonthly);
